c1555ea9edde3a3968bb9a4a7bb33cd8e2dd7be6
OoSnowfxm <64147969+OoSnowfxm@users.noreply.github.com>
2021-12-01 21:28:54 +0800

    second commit

--- AlphaGoZero/code/Learn.py
+++ AlphaGoZero/code/Learn.py
@@ -8,28 +8,31 @@ import logging
 import os
 import numpy as np
 from collections import deque
-from pickle import Pickler, Unpickler
 from random import shuffle
+
+import numpy as np
 from tqdm import tqdm
-from SelfPlay import SelfPlay
+
 from Framework.Mcts import MCTS
 from Framework.Net import dotdict
+from SelfPlay import SelfPlay
 
 log = logging.getLogger(__name__)
 
 args = dotdict({
-    'numIters': 100,              # 神经网络训练迭代次数
-    'numEps': 100,                # 自学习过程每次比赛场数
-    'tempThreshold': 15,        # 搜索树的阈值
-    'updateThreshold': 0.5,     # 如果新模型赢了超过updateThreshold比例的比赛，就接受新模型
-    'maxlenOfQueue': 200000,    # 训练数据上限
-    'numMCTSSims': 25,          # 搜索树采样数量
-    'arenaCompare': 2,         # 新旧模型对比需要的比赛次数
-    'cpuct': 1,                 # 搜索树参数
-    'checkpoint': './Model/',   # 模型保存路径
-    'load_model': False,        # 是否加载模型
+    'numIters': 100,  # 神经网络训练迭代次数
+    'numEps': 100,  # 自学习过程每次比赛场数
+    'tempThreshold': 15,  # 搜索树的阈值
+    'updateThreshold': 0.5,  # 如果新模型赢了超过updateThreshold比例的比赛，就接受新模型
+    'maxlenOfQueue': 200000,  # 训练数据上限
+    'numMCTSSims': 25,  # 搜索树采样数量
+    'arenaCompare': 2,  # 新旧模型对比需要的比赛次数
+    'cpuct': 1,  # 搜索树参数
+    'checkpoint': './Model/',  # 模型保存路径
+    'load_model': False,  # 是否加载模型
     'numItersFortable': 20,
 })
+
 
 class Learn():
     '''
@@ -42,20 +45,23 @@ class Learn():
         args：其他参数
         table：保存的训练数据，每次训练都使用这里面的数据
     '''
+
     def __init__(self, game, net, args):
         self.game = game
         self.net = net
-        self.pnet = self.net.__class__(self.game)  
+        self.pnet = self.net.__class__(self.game)
         self.args = args
         self.mcts = MCTS(self.game, self.net, self.args)
-        self.table = []  
+        self.table = []
         self.firstrain = True
+
     '''
         process过程算法
         将蒙特卡洛树搜索得到的
         棋盘(即状态)、得到的action概率向量、值
         传入神经网络进行学习
     '''
+
     def process(self):
         # 结果保存表
         table = []
@@ -77,7 +83,7 @@ class Learn():
             sym = self.game.getSymmetries(canonicalBoard, prob)
             for b, p in sym:
                 table.append([b, self.curcolor, p, None])
-            
+
             # 执行动作后获得下一状态，并交换掌棋权，然后继续执行上述过程
             action = np.random.choice(len(prob), p=prob)
             board, self.curcolor = self.game.getNextState(board, self.curcolor, action)
@@ -88,20 +94,21 @@ class Learn():
             # 状态本身，动作action概率向量，以及最有希望得到的值
             if ended != -2:
                 return [(x[0], x[2], ended * ((-1) ** (x[1] != self.curcolor))) for x in table]
-    
+
     '''
         学习过程
         AC模块采用神经网络学习
         process模块采用蒙特卡洛树搜索过程
     '''
+
     def learn(self):
-        for i in range(1, self.args.numIters+1):
+        for i in range(1, self.args.numIters + 1):
             log.info(f'Starting Iter #{i} ...')
             deq = deque([], maxlen=self.args.maxlenOfQueue)
 
             # 在自学习过程中，执行process算法过程
             for _ in tqdm(range(self.args.numEps), desc="Self Play"):
-                self.mcts = MCTS(self.game, self.net, self.args)  
+                self.mcts = MCTS(self.game, self.net, self.args)
                 deq += self.process()
 
             # 将训练结果保存到table中
@@ -116,7 +123,6 @@ class Learn():
             for e in self.table:
                 data.extend(e)
             shuffle(data)
-
 
             # 训练新的网络前需要保存旧的网络
             self.net.saveCheckpoint(folder=self.args.checkpoint, filename='temp.pth.tar')
@@ -135,7 +141,7 @@ class Learn():
 
             log.info('与旧的网络对比中：')
             arena = SelfPlay(lambda x: np.argmax(pmcts.getActionProb(x, temp=0)),
-                        lambda x: np.argmax(mcts.getActionProb(x, temp=0)), self.game)
+                             lambda x: np.argmax(mcts.getActionProb(x, temp=0)), self.game)
             pwins, wins, draws = arena.playGames(self.args.arenaCompare)
 
             log.info('胜/负 : %d / %d ; 平局 : %d' % (wins, pwins, draws))
